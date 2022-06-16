fe0543420ca06f45d569d2e7a0cc5827df556941
Saahith Janapati <itzsaahith01@gmail.com>
2022-06-16 12:09:37 -0400

    get track information using spotipy

---
+++ get_data.py
@@ -0,0 +1,15 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from config import CLIENT_ID, CLIENT_SECRET
+
+client_credentials_manager = SpotifyClientCredentials(CLIENT_ID, CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+playlists = sp.user_playlists('spotify')
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+    if playlists['next']:
+        playlists = sp.next(playlists)
+    else:
+        playlists = None
